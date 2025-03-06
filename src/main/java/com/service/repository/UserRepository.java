3e3fe992dae37dc6aeac24f5e32a0c29af5dce9c
Oscar Diaz <oscar@MacBook-Pro-de-Oscar.local>
2025-03-05 21:59:18 -0600

    Upgrade to java 17

--- src/main/java/com/service/repository/UserRepository.java
+++ src/main/java/com/service/repository/UserRepository.java
@@ -1,6 +1,6 @@
-package com.staxrt.tutorial.repository;
+package com.service.repository;
 
-import com.staxrt.tutorial.model.User;
+import com.service.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
