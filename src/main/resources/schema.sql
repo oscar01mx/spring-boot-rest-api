e06d1e56899dfca7d547d0c738a1a67b0db43562
Oscar Diaz <oscar@MacBook-Pro-de-Oscar.local>
2025-03-06 13:55:53 -0600

    Upgrade to java 17

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,12 +1,10 @@
-CREATE TABLE users
-(
-    id            BIGINT       NOT NULL,
-    first_name    VARCHAR(255) NOT NULL,
-    last_name     VARCHAR(255) NOT NULL,
-    email_address VARCHAR(255) NOT NULL,
-    created_at    datetime     NOT NULL,
-    created_by    VARCHAR(255) NOT NULL,
-    updated_at    datetime     NOT NULL,
-    updated_by    VARCHAR(255) NOT NULL,
-    CONSTRAINT pk_users PRIMARY KEY (id)
+CREATE TABLE users (
+                       id BIGINT AUTO_INCREMENT PRIMARY KEY,
+                       first_name VARCHAR(255) NOT NULL,
+                       last_name VARCHAR(255) NOT NULL,
+                       email_address VARCHAR(255) NOT NULL,
+                       created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+                       created_by VARCHAR(255) NOT NULL,
+                       updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NOT NULL,
+                       updated_by VARCHAR(255) NOT NULL
 );
